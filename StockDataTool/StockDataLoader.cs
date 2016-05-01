d1329fdfec80838767a574a513b1aca6b786ecc0
Dmitry <dmitry@me.com>
2016-05-01 22:34:43 +0200

    Defined the StockDataLoader class

---
+++ StockDataTool/StockDataLoader.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace StockDataTool
+{
+    class StockDataLoader
+    {
+        //http://financials.morningstar.com/valuation/price-ratio.html?t=AAPL
+
+        public static void DownloadPricesCsv(string ticker)
+        {
+            //http://real-chart.finance.yahoo.com/table.csv?s=AAPL&a=00&b=1&c=2006&d=11&e=31&f=2015&g=d&ignore=.csv
+        }
+
+    }
+}
