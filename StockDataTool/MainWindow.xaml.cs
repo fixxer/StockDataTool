470ae8c3ffae4facec0c8f4d6f0f922ea566fb72
Dmitry <ransom@hse.su>
2016-05-30 14:12:07 +0200

    add UI for traking progress

--- StockDataTool/MainWindow.xaml.cs
+++ StockDataTool/MainWindow.xaml.cs
@@ -1,54 +1,88 @@
 ﻿
 using System;
 using System.Windows;
+using System.ComponentModel;
 
 
 namespace StockDataTool
 {
     public partial class MainWindow : Window
     {
+        BackgroundWorker bw;
+
         public MainWindow()
         {
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.DoWork += Bw_DoWork;
+            bw.ProgressChanged += Bw_ProgressChanged;
             InitializeComponent();
+            progressBar.Maximum = 150;
+            bw.RunWorkerAsync();
+        }
 
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            logTextBox.Text += e.UserState.ToString();
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
             //0. Downloading available tickers
+            bw.ReportProgress(10, "Downloading available tickers");
             StockDataLoader.GetAllTickers();
+            bw.ReportProgress(20, "...done!\r\n");
 
             // 1. Building Portfolio: Tickers. Curerntly the list is pre-defined, may be switched to downloaded at {0}
+            bw.ReportProgress(30, "Building portfolio");
             Portfolio portfolio = new Portfolio();
+            bw.ReportProgress(40, "...done!\r\n");
 
             //2. Downloading historical prices data
+            bw.ReportProgress(50, "Downloading historical prices");
             foreach (Stock stock in portfolio.Stocks)
             {
                 var longPath = StockDataLoader.DownloadPricesCsv(stock.Ticker, 2006, 2015);
                 var shortPath = StockDataLoader.ReformatPricesCsv(longPath);
                 StockDataLoader.CreateHistoricalData(stock, shortPath, ref portfolio);
             }
+            bw.ReportProgress(60, "...done!\r\n");
 
+            bw.ReportProgress(70, "Enriching stocks with AAR data");
             //3. Enriching stocks with AAR data
             StockDataLoader.EnrichStocksWithAAR(portfolio);
+            bw.ReportProgress(80, "...done!\r\n");
+
 
             //4. Enriching stocks with STDs
+            bw.ReportProgress(90, "Enriching stocks with STDs");
             StockDataLoader.EnrichStocksWithSTD(portfolio);
+            bw.ReportProgress(100, "...done!\r\n");
 
             //5. Enriching stocks with P/Es
+            bw.ReportProgress(110, "Enriching PEs");
             foreach (Stock stock in portfolio.Stocks)
             {
                 StockDataLoader.GetMorningstarData(stock);
             }
+            bw.ReportProgress(120, "...done!\r\n");
 
             /*n. Generating output. Currently contains:
                 - raw data in rows
                 - ticker
                 - AAR
             */
+            bw.ReportProgress(130, "Generating xls");
             StockDataLoader.GenerateMySpreadsheet(ref portfolio);
-            this.Close();
+            bw.ReportProgress(140, "...done!\r\n");
+
+            bw.ReportProgress(150, "All done!\r\n");
         }
 
         private void goButton_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
 
 
         }
